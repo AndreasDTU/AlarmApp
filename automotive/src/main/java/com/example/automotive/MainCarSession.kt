531259677d97ba1762beaf8ebb8730f9048ca344
AndreasDTU <s235455@dtu.dk>
2025-11-19 10:50:55 +0100

    Added base framework

---
+++ automotive/src/main/java/com/example/automotive/MainCarSession.kt
@@ -0,0 +1,4 @@
+package com.example.automotive
+
+class MainCarSession {
+}
---
+++ shared/src/main/java/com/example/shared/AlarmViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.shared
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class AlarmViewModel(
+    private val channelId: Int,
+    private val apiKey: String
+) : ViewModel() {
+
+    val alarms = AlarmRepository.alarms
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                AlarmRepository.updateAlarms(channelId, apiKey)
+                delay(10_000)  // fetch every 10 seconds
+            }
+        }
+    }
+}
