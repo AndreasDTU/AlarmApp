e37c5f8b45cba6d7ce8adda8662026481e803156
Danish-Pirate <arjacokaki@hotmail.com>
2025-11-26 09:59:26 +0100

    Commit 2

--- app/src/main/java/com/example/alarmapp/DataModel.kt
+++ app/src/main/java/com/example/alarmapp/DataModel.kt
@@ -1,7 +1,9 @@
 package com.example.alarmapp
 
 data class DataModel(
-    val id: Int,
-    val name: String,
-    val description: String
+    val freezer_number: Int,
+    val alarm_code: String,
+    val alarm_desc: String,
+    val alarm_type: Int,
+    val temperature: Int
 )
