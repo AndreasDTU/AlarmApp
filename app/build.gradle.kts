93d062f3276f34b811117206f9d12e768ae8d761
AndreasDTU <s235455@dtu.dk>
2025-11-26 11:23:25 +0100

    All new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.alarmapp"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.alarmapp"
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.shared"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
 
