d711c86e89e86e603e3528354aab7f554ffdd0d0
Danish-Pirate <91475695+Danish-Pirate@users.noreply.github.com>
2025-11-27 20:54:26 +0100

    Improved UX and code cleanup.

--- app/src/main/java/com/example/alarmapp/DataModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alarmapp
-
-data class DataModel(
-    val freezer_number: Int,
-    val alarm_code: String,
-    val alarm_desc: String,
-    val alarm_type: Int,
-    val temperature: Int
-)
--- app/src/main/java/com/example/alarmapp/Screens/AlarmListScreen.kt
+++ app/src/main/java/com/example/alarmapp/Screens/AlarmListScreen.kt
@@ -1,75 +1,56 @@
 package com.example.alarmapp.Screens;
 
-import android.app.Activity
-import android.inputmethodservice.Keyboard
-import androidx.car.app.model.ItemList
-import androidx.car.app.model.ListTemplate
-import androidx.car.app.model.Template
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.shared.Alarm
 import com.example.shared.AlarmViewModel
-import com.example.shared.AlarmRepository
 
 @Composable
 fun AlarmListScreen(
     trailerId: Int,
-    viewModel: AlarmViewModel
+    viewModel: AlarmViewModel,
+    onBackClick: () -> Unit
 ) {
-    val context = LocalContext.current
-    val activity = context as? Activity
     // Load alarms when the screen is first composed
     LaunchedEffect(trailerId) {
         viewModel.loadAlarms(trailerId)
     }
 
     val alarms = viewModel.alarms
-
-    if (alarms.isEmpty()) {
-        // Show "No alarms" when the list is empty
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "No alarms")
-        }
-    } else {
-        // Show the list of alarms
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
+    Column(Modifier.padding(16.dp)) {
+        Button(
+            onClick = onBackClick , // Ends this activity and goes back
         ) {
-            items(alarms) { alarm ->
-                AlarmItem(alarm)
+            Text("Back")
+        }
+
+        if (alarms.isEmpty()) {
+            // Show "No alarms" when the list is empty
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "No alarms")
+            }
+        } else {
+            // Show the list of alarms
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(alarms) { alarm ->
+                    AlarmItem(alarm)
+                }
             }
         }
-    }
-    Button(
-        onClick = { activity?.finish() }, // Ends this activity and goes back
-        modifier = Modifier.padding(16.dp)
-    ) {
-        Text("Back")
     }
 }
 
